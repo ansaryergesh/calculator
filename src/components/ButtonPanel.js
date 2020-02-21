50ad47adeed37964d0de778204778f266f9316ae
ansaryergesh <yergeshovansar@gmail.com>
2020-02-22 02:11:23 +0600

    button panel component started

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -3,7 +3,35 @@ import Button from './Button';
 
 const ButtonPanel = () => (
     <div className ="ButtonPanel">
-        <Button name="button" />
+      <div className = "Button-group">
+        <Button name="AC" />
+        <Button name="+/-" />
+        <Button name="%" />
+        <Button name="÷" />
+      </div>
+      <div className = "Button-group">
+        <Button name="7" />
+        <Button name="8" />
+        <Button name="9" />
+        <Button name="x" />
+      </div>
+      <div className = "Button-group">
+        <Button name="4" />
+        <Button name="5" />
+        <Button name="6" />
+        <Button name="-" />
+      </div>
+      <div className = "Button-group">
+        <Button name="1" />
+        <Button name="2" />
+        <Button name="3" />
+        <Button name="+" />
+      </div>
+      <div className = "Button-group">
+        <Button name="0" />
+        <Button name="." />
+        <Button name="=" />
+      </div>
     </div>
 );
 
