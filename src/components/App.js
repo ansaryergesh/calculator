d28838ca7805218a75c39997c38bc7167be43306
ansaryergesh <yergeshovansar@gmail.com>
2020-02-21 22:13:29 +0600

    checking jsx

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
+import './App.css';
+class App extends Component  {
+    render() {
+        return (
+            <div className='App'>
+                <Display/>
+                <ButtonPanel/>
+            </div>
+        )
+    }
+}
---
+++ src/components/Button.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import './Button.css';
+
+const 
