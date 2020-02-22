5641122360d16dbacd234ba4949d88b353c6021d
ansaryergesh <yergeshovansar@gmail.com>
2020-02-22 06:35:19 +0600

    styled button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = (props) => {
-  const { name } = props;
+const Button = ({ name, color, wide }) => {
+  const style = {
+    wide: wide? '50%' : '25%',
+    backgroundColor: color,
+  }
   return (
-    <button type="button">
+    <button type="button" style={style}>
       {name}
     </button>
   );
 };
   
 Button.propTypes = {
-  name: PropTypes.string.isRequired,
+  name: PropTypes.string,
+  wide: PropTypes.bool,
+  color: PropTypes.string,
 };
+
+Button.defaultProps = {
+  wide: false,
+  color: 'gray',
+}
   
 export default Button;
