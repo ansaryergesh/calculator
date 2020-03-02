2f0cc56ab5aae744b18ff72e7123495d5591b309
ansaryergesh <yergeshovansar@gmail.com>
2020-03-02 06:16:37 +0600

    operate module

---
+++ src/logic/operator.js
@@ -0,0 +1,24 @@
+import Big from 'big.js';
+
+const operate = (num1, num2, operation) => {
+  const n1 = Big(num1);
+  const n2 = Big(num2);
+  switch (operation) {
+    case '+':
+      return n1.plus(n2);
+    case '-':
+      return n1.minus(n2);
+    case 'X':
+      return n1.times(n2);
+    case '÷':
+      if (n2 === 0) {
+        break;
+      }
+      return n1.div(n2);
+    default:
+      return '0';
+  }
+};
+
+
+export default operate;
