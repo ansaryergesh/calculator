808e0bf18c8d9360e146f6524b74b520eca89e6b
ansaryergesh <yergeshovansar@gmail.com>
2020-03-02 06:48:29 +0600

    continued calculate model

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,3 +1,35 @@
-const calculate = (object, buttonName) => {
+import operate from './operate';
 
+const calculate = (object, buttonName) => {
+  const operationSymbols = ['+', '-', '%', '÷', 'X'];
+  switch (buttonName) {
+    case 'AC':
+      return {
+        total: null,
+        next: null,
+        operation: null,
+      };
+  }
+  case '+/-':
+      if(object.next){
+      return{
+        next: next * -1;
+      }
+    }
+  case '+':
+  case '-':
+  case 'x':
+  case '÷':
+    return {
+        total: operate(object.total, object.next, object.opeation),
+        next: '',
+        opeation: buttonName,
+    }
+  case '=':
+    return {
+        total: operate(object.total, object.next, object.opeation),
+        next: '',
+        opeation: '',
+    }
+       
 };
